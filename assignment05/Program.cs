1bc0caf3bdc7ef1d542e1e0ffb851b5a70481c36
leer89 <leer89@gmail.com>
2017-02-27 15:02:15 -0500

    - problem 1 Write a program that generates 1000 random numbers between 0 and 100000. Display the number of odd values generated as well as the smallest and the largest of values. Output should be displayed in a Windows message box.

--- assignment05/Program.cs
+++ assignment05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace assignment05
 {
@@ -10,6 +11,41 @@ namespace assignment05
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            int odds = 0;
+            int evens = 0;
+            int x;
+            int min = 100001;
+            int max = 0;
+            string result = "min \tmax \todds \tevens\n";
+            Random rnd = new Random();
+            while (i < 1000)
+            {
+                i++;
+
+                x = rnd.Next(1, 100000);
+                if (x < min)
+                    min = x;
+                if (x > max)
+                    max = x;
+
+                if ((x % 2) == 0)
+                    evens++;
+                else
+                    odds++;
+
+            }
+
+            // message box contents
+            result += min
+                + "\t"
+                + max
+                + "\t"
+                + odds
+                + "\t"
+                + evens;
+
+            MessageBox.Show(result, "Problem 1 - min, max, odds, evens");
         }
     }
 }
